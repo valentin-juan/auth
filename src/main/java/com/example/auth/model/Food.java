0474f21d6c9e55e9fe653f7ec78268e3a4a31ee9
Juan Valentin <valentin.juan05@gmail.com>
2025-06-11 21:11:28 -0300

    first files to auth project

---
+++ src/main/java/com/example/auth/model/Food.java
@@ -0,0 +1,7 @@
+package com.example.auth.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Food {
+}
---
+++ src/main/java/com/example/auth/repository/FoodRepository.java
@@ -0,0 +1,4 @@
+package com.example.auth.repository;
+
+public class FoodRepository {
+}
